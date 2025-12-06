14665af207b8975c1306d43f0e1868f5f9a091a9
huranajesus-ship-it <hurana.jesus@academico.uncisal.edu.br>
2025-12-05 22:13:03 -0300

    Add files via upload

---
+++ user.js
@@ -0,0 +1,23 @@
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    photoUrl: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  });
+
+  return User;
+};
